1ead6645f7bae64eb5569ac83af1011bc62885c7
donalconn <38005567+donalconn@users.noreply.github.com>
2018-09-06 10:25:31 +0100

    Fix up

--- chessboard.js
+++ chessboard.js
@@ -1,16 +1,16 @@
-var size = 8; //this is the variable setting
+var size = 8;
 
-var board = ""; //this is the empty string we're going to add either ' ' , '#' or newline
+var board = "";
 
-for (var y = 0; y < size; y++) { /*in the outer loop we add newline to seperate rows*/
-  for (var x = 0; x < size; x++) { /*every inner loop rappresents a line, and alternatively it's adding either ' ' or '#' to the string that's being populated*/
+for (var y = 0; y < size; y++) { 
+  for (var x = 0; x < size; x++) { 
     if ((x + y) % 2 == 0)
       board += " ";
     else
       board += "#";
   }
+
   board += "\n";
 }
-
 
 console.log(board);
