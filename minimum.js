ac40ef726fe17a7328c854d9bf707406f97ebf6a
donalconn <38005567+donalconn@users.noreply.github.com>
2018-09-06 13:02:49 +0100

    Compled sum of a range

--- minimum.js
+++ minimum.js
@@ -1,10 +1,10 @@
-console.log(min(0, 10)); // 0
-console.log(min(0, -10)); // -10
-
-
 function min(a, b) {
     if (a < b) {
         return a;
-    }  
+    }
     return b;
 }
+
+console.log(min(0, 10)); // 0
+
+console.log(min(0, -10)); // -10
--- sumofarange.js
+++ sumofarange.js
@@ -1,77 +1,31 @@
-// // @ts-check
+var arrayToReverse = ['A', 'B', 'C'];
 
-// // var array1 = ['a', 'b', 'c'];
+var anotherArrayToReverse = ['A', 'B', 'C', 'D', 'E'];
 
-// // array1.forEach(function(element) {
-// //   console.log(element);
-// // });
+console.log(reverseArray(arrayToReverse));
+// [ 'C', 'B', 'A' ]
+console.log(reverseArrayInPlace(anotherArrayToReverse))
+// [ 'E', 'D', 'C', 'B', 'A' ]
 
-// // expected output: "a"
-// // expected output: "b"
-// // expected output: "c"
+function reverseArray(array) {
 
-// function range(start, end, step = 1) {
-//   if (end < step && step > 0) {
-//     step = -1;
-//   }
+  var newArray = [];
 
-//   var rangeArray = [];
-
-//   if (step < 1) {
-//     for (start; start >= end; start = start + step) {
-//       rangeArray.push(start);
-//     }
-//   } else {
-//     for (start; start <= end; start = start + step) {
-//       rangeArray.push(start);
-//     }
-//   }
-
-//   return rangeArray;
-// }
-
-// function sum(array) {
-//   var sum = 0;
-//   array.forEach(element => {
-//     sum += element;
-//   });
-
-//   return sum;
-// }
-
-// console.log(range(1, 10));
-// // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log(range(1, 10, 2));
-// // → [ 1, 3, 5, 7, 9 ]
-// console.log(range(5, 2, -1));
-// // // → [5, 4, 3, 2]
-// console.log(sum(range(1, 10)));
-// // // → 55
-// console.log(range(5, 2));
-
-// // → [5, 4, 3, 2]
-// var myArray = [5, 4, 3, 3];
-
-// function reverseArray(myArray) {
-//   var newArray = [];
-
-//   for (let index = myArray.length - 1; index > -1; index--) {
-//     const element = myArray[index];
-//     newArray.push(element);
-//   }
-
-//   return newArray;
-// }
-
-var reverse = [1, 2, 3, 4];
-
-// console.log(reverseArray(myArray));
+  for (let i = array.length - 1; i >= 0; i--) {
+    const element = array[i];
+    newArray.push(element);
+  }
+  return newArray;
+}
 
 function reverseArrayInPlace(array) {
-  for (let index = 0; index < array.length; index++) {
-    last = array.pop;
-    array.unshift(last);
+
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+
+    var old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
   }
+
   return array;
 }
-console.log(reverseArrayInPlace(reverse));
